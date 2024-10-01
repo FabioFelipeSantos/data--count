b60072db94e2c64594f34a0020ff807763066553
Fábio Santos <109525700+FabioFelipeSantos@users.noreply.github.com>
2024-10-01 04:26:19 +0000

    feat: adding step

--- data-counter/src/App.jsx
+++ data-counter/src/App.jsx
@@ -1,9 +1,36 @@
-function App() {
-	return (
+import { useState } from "react";
+
+export default function App() {
+  return (
     <div>
-      <h1>FÁBIO</h1>
+      <h1>Fabio</h1>
+      <Step />
     </div>
-  )
+  );
 }
 
-export default App
+function Step() {
+  const [step, setStep] = useState(0);
+
+  function handleMinus() {
+    if (step > 0) setStep((s) => s - 1);
+  }
+
+  function handleAdd() {
+    setStep((s) => s + 1);
+  }
+
+  return (
+    <div className="step">
+      <button className="step__subtract" onClick={handleMinus}>
+        -
+      </button>
+      <p className="step__text">Step</p>
+      <button className="step__add" onClick={handleAdd}>
+        +
+      </button>
+
+      <p>{step}</p>
+    </div>
+  );
+}
